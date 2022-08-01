c1ab5e3e04a3150424d5cef8f12c07b7d156d063
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2022-08-01 16:22:56 +0200

    TASK: add explanation about ConcurrencyException in EventStoreInterface

--- src/EventStoreInterface.php
+++ src/EventStoreInterface.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 namespace Neos\EventStore;
 
+use Neos\EventStore\Exception\ConcurrencyException;
 use Neos\EventStore\Model\EventStore\CommitResult;
 use Neos\EventStore\Model\EventStream\EventStreamInterface;
 use Neos\EventStore\Model\EventStream\ExpectedVersion;
@@ -12,6 +13,14 @@ use Neos\EventStore\Model\Events;
 interface EventStoreInterface
 {
     public function load(StreamName|VirtualStreamName $streamName): EventStreamInterface;
+
+    /**
+     * @param StreamName $streamName
+     * @param Events $events
+     * @param ExpectedVersion $expectedVersion
+     * @return CommitResult
+     * @throws ConcurrencyException in case the expectedVersion does not match
+     */
     public function commit(StreamName $streamName, Events $events, ExpectedVersion $expectedVersion): CommitResult;
     public function deleteStream(StreamName $streamName): void;
 }
