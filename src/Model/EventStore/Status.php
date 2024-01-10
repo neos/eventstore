ef0f21425d925ae1046b5bfc6313c54fa2dc9c79
Bastian Waidelich <b.waidelich@wwwision.de>
2024-01-10 18:48:04 +0100

    FEATURE: Inline ProvidesSetupInterface and ProvidesStatusInterface and overhaul return types
    Resolves: #16

--- src/Model/EventStore/Status.php
+++ src/Model/EventStore/Status.php
@@ -2,38 +2,19 @@
 declare(strict_types=1);
 namespace Neos\EventStore\Model\EventStore;
 
-use Neos\EventStore\ProvidesStatusInterface;
-
 /**
- * The result of a {@see ProvidesStatusInterface::status()} call
+ * The result of a {@see EventStoreInterface::status()} call
  * @api
  */
 final class Status
 {
     /**
-     * @param string[] $errors
-     * @param string[] $warnings
-     * @param string[] $notices
+     * @param StatusType $type The type of status
+     * @param string $details Further technical details about the status
      */
     private function __construct(
-        public readonly array $errors,
-        public readonly array $warnings,
-        public readonly array $notices,
+        public readonly StatusType $type,
+        public readonly string $details,
     ) {
-    }
-
-    public static function success(string $notice = null): self
-    {
-        return new self([], [], $notice !== null ? [$notice] : []);
-    }
-
-    public static function error(string $error): self
-    {
-        return new self([$error], [], []);
-    }
-
-    public static function warning(string $warning): self
-    {
-        return new self([], [$warning], []);
     }
 }
